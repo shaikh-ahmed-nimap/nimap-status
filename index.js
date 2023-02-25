b72d2dcda4dfd3dfd0b8758b86ac48764c5aca47
shaikh-ahmed-nimap <124054863+shaikh-ahmed-nimap@users.noreply.github.com>
2023-02-25 18:26:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-function sencondHigh (arr) {
-    let hieghest = -Infinity;
-    let second = Infinity;
-    for (i of arr) {
-        if (i > hieghest) {
-            second = hieghest;
-            hieghest = i
-        } else if (i < hieghest && i > second) {
-            second = i
-        }else {
-            continue;
-        }
+import Redis from 'redis';
+import fetch from 'node-fetch';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors())
+
+const client = Redis.createClient();
+
+client.connect();
+
+client.on('connect', (err) => {
+    if (err) {
+        console.log(err);
+        throw err;
     }
-    return second;
-}
+    console.log('client connected')
+})
 
-console.log(sencondHigh([2, 5, 3, 6]))
+app.get('/photos', async (req, res) => {
+    try {
+        console.log(req.url)
+        let data;
+        const cachedData = await client.get('photos');
+        if (!cachedData) {
+            const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+            const data = await response.json();
+            await client.SETEX('photos', 3600, JSON.stringify(data));
+            return res.status(200).json({source: 'api', data})
+        };
+        console.log("end of redis get");
+        return res.status(200).json({source: 'cached', data: JSON.parse(cachedData)})
+    } catch (e) {
+        console.log(e);
+        return res.send('error')
+    }
 
+})
+
+app.listen(5000, () => {
+    console.log('server listening 5000');
+})
 
